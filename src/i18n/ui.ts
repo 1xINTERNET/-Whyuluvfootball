dafa0a860af5146de1c02e6e09323f960cf74471
John Durance <j.durance@1xinternet.de>
2024-06-27 16:11:25 +0100

    #25: Add i18n

---
+++ src/i18n/ui.ts
@@ -0,0 +1,23 @@
+export const languages = {
+  de: "Deutch",
+  en: "English",
+};
+
+export const defaultLang = "de";
+
+export const ui = {
+  de: {
+    "nav.goals": "Unsere Goals",
+    "nav.videos": "Videos",
+    "nav.partners": "Partner",
+    "nav.help": "Mithelfen",
+    "nav.about": "Über uns",
+  },
+  en: {
+    "nav.goals": "Our goals",
+    "nav.videos": "Videos",
+    "nav.partners": "Partners",
+    "nav.help": "How to help",
+    "nav.about": "About us",
+  },
+} as const;
